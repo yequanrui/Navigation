8eb5db6e20fb9adf8ea83b12549cf61a2d98aacf
yequanrui <yequanrui@qq.com>
2017-04-04 14:12:12 +0800

    compress resources
    Former-commit-id: 2588df298a1a2f9141fa011f010a80b5c1ca4739

--- Navigation/Properties/Resources.Designer.cs
+++ Navigation/Properties/Resources.Designer.cs
@@ -411,16 +411,6 @@ namespace Navigation.Properties {
         }
         
         /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
-        internal static System.Drawing.Bitmap 学校卫星图 {
-            get {
-                object obj = ResourceManager.GetObject("学校卫星图", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   查找类似于 (Icon) 的 System.Drawing.Icon 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Icon 校徽 {
